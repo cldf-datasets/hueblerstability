7888c06cd697981beeafabda629e9555d92d1fa0
Christoph Rzymski <christoph@functor-argument.net>
2021-02-10 10:31:07 +0100

    Add data

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # hueblerstability
+
+
+Some description
+
+This dataset is licensed under a CC-BY-4.0 license
--- cldfbench_hueblerstability.py
+++ cldfbench_hueblerstability.py
@@ -1,26 +1,49 @@
 import pathlib
 
+from cldfbench import CLDFSpec
 from cldfbench import Dataset as BaseDataset
+from pycldf import StructureDataset
 
 
 class Dataset(BaseDataset):
     dir = pathlib.Path(__file__).parent
-    id = "hueblerstability"
+    id = "huebler"
 
-    def cldf_specs(self):  # A dataset must declare all CLDF sets it creates.
-        return super().cldf_specs()
+    def cldf_specs(self):
+        return CLDFSpec(
+            dir=self.cldf_dir,
+            module="StructureDataset",
+            default_metadata_path=self.raw_dir / "StructureDataset-metadata.json",
+        )
 
     def cmd_download(self, args):
-        """
-        Download files to the raw/ directory. You can use helpers methods of `self.raw_dir`, e.g.
-
-        >>> self.raw_dir.download(url, fname)
-        """
         pass
 
     def cmd_makecldf(self, args):
-        """
-        Convert the raw data to a CLDF dataset.
+        raw_ds = StructureDataset.from_metadata(self.raw_dir / "StructureDataset-metadata.json")
+        args.writer.cldf.add_component("ExampleTable")
 
-        >>> args.writer.objects['LanguageTable'].append(...)
-        """
+        for row in raw_ds["LanguageTable"]:
+            args.writer.objects["LanguageTable"].append(row)
+
+        for row in raw_ds["ParameterTable"]:
+            args.writer.objects["ParameterTable"].append(row)
+
+        for row in raw_ds["ValueTable"]:
+            args.writer.objects["ValueTable"].append(row)
+
+        for row in raw_ds["CodeTable"]:
+            args.writer.objects["CodeTable"].append(row)
+
+        for example in self.raw_dir.read_csv("examples.csv", dicts=True):
+            args.writer.objects["ExampleTable"].append(
+                dict(
+                    ID=example["ID"],
+                    Language_ID=example["Language_ID"],
+                    Primary_Text=example["Primary_Text"],
+                    Analyzed_Word=example["Analyzed_Word"],
+                    Gloss=example["Gloss"],
+                    Translated_Text=example["Translated_Text"],
+                    Comment=example["Comment"],
+                )
+            )
