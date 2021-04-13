4284312869222ffd31665f84748572001de69a27
Robert Forkel <xrotwang@googlemail.com>
2021-04-13 09:00:43 +0200

    cldf now created from raw raw data

--- cldfbench_hueblerstability.py
+++ cldfbench_hueblerstability.py
@@ -1,15 +1,41 @@
 import re
 import pathlib
+import itertools
 import collections
 
+import attr
 from cldfbench import CLDFSpec
 from cldfbench import Dataset as BaseDataset
-from pycldf import StructureDataset
 
 REF_PATTERNS = [
     re.compile(r'(?P<author>[^(]+)\((?P<year>[^:)]+)(:(?P<pages>.*))?\)'),
     re.compile(r'(?P<author>[^\s]+)\s+(?P<year>[0-9]{4})'),
 ]
+
+
+@attr.s
+class Reference:
+    author = attr.ib(converter=lambda s: s.strip())
+    year = attr.ib(converter=lambda s: s.strip())
+    pages = attr.ib(converter=lambda s: s.strip() if s else s, default=None)
+
+    @property
+    def key(self):
+        return self.author, self.year
+
+    def as_cldf(self, key):
+        res = key
+        if self.pages:
+            res += '[{}]'.format(self.pages)
+        return res
+
+
+def match_ref(ref):
+    ref = ref.strip()
+    for p in REF_PATTERNS:
+        m = p.fullmatch(ref)
+        if m:
+            return Reference(**m.groupdict())
 
 
 class Dataset(BaseDataset):
@@ -23,36 +49,23 @@ class Dataset(BaseDataset):
         )
 
     def cmd_download(self, args):
-        purge = [
-        "TE001", "TE002", "TE009", "TE012", "TE014", "TE015", "TE016", "TE022", "TE025",
-        "TE026", "TE028", "TE029", "TE033", "TE034", "TE036", "TE040", "TE041", "TE042",
-        "TE043", "TE044", "TE045", "TE046", "TE047", "TE048", "TE049", "TE051", "TE055",
-        "TE056", "TE057", "TE058", "TE060", "TE061", "TE062", "TE063", "TE064", "TE065",
-        "TE067", "TE068", "TE069", "TE070", "TE071", "TE072", "TE073", "TE074", "TE076",
-        "TE077", "TS081", "TS082", "TS083", "TS083", "TS084", "TS085"]
-        
         pass
 
     def cmd_makecldf(self, args):
         self._schema(args.writer.cldf)
         glottolog = args.glottolog.api
-        sources = {(r['author'], r['year']): r for r in self.etc_dir.read_csv('sources.csv', dicts=True)}
-        #
-        # FIXME: fetch bibtex for sources from Glottolog!
-        #
-        codes = collections.defaultdict(collections.Counter)
+        sources = {
+            (r['author'], r['year']): r for r in self.etc_dir.read_csv('sources.csv', dicts=True)}
+        examples = {eid: list(ex) for eid, ex in itertools.groupby(
+            sorted(self.etc_dir.read_csv('examples.csv', dicts=True), key=lambda r: r['ID']),
+            lambda r: r['ID']
+        )}
+        for eid, ex in examples.items():
+            for i, e in enumerate(ex, start=1):
+                e['ID'] = '{}-{}'.format(e['ID'], i)
         missing = collections.Counter()
-        COLS = [
-            'Grambank ID',
-            'Feature',
-            'Possible Values',
-            'Value',
-            'Source',
-            'Comment',
-            'Detailed comment',
-            'Example',
-        ]
-        pids = set()
+        unknown = collections.Counter()
+        pids, srcids, codes = set(), set(), collections.defaultdict(set)
         for sheet in self.raw_dir.glob('*.tsv'):
             m = re.fullmatch(r'(?P<name>[^\[]+)\[(?P<gc>[a-z0-9]{8})]', sheet.stem)
             lname, gc = m.group('name').strip(), m.group('gc')
@@ -64,38 +77,67 @@ class Dataset(BaseDataset):
             values = self.raw_dir.read_csv(sheet.name, delimiter='\t', dicts=True)
             assert values
             for val in values:
-                codes[val['Grambank ID']].update([val['Value']])
-                assert val['Grambank ID'], str(val)
+                assert val['Grambank ID'] and val['Value'], str(val)
+                codes[val['Grambank ID']].add(val['Value'])
                 if val['Grambank ID'] not in pids:
                     args.writer.objects['ParameterTable'].append(dict(
                         ID=val['Grambank ID'],
                         Name=val['Feature'],
                     ))
                     pids.add(val['Grambank ID'])
+
+                refs = []
                 for ref in val['Source'].split(';'):
-                    ref = ref.strip()
-                    if ref:
-                        for p in REF_PATTERNS:
-                            m = p.fullmatch(ref)
-                            if m:
-                                key = (m.group('author').strip(), m.group('year').strip())
-                                if key not in sources:
-                                    missing.update([key])
-                                    print('---', ref)
-                                break
-                        else:
-                            print(ref)
+                    reference = match_ref(ref)
+                    if reference:
+                        if reference.key not in sources:
+                            missing.update([reference.key])
+                        elif sources[reference.key]['matchid']:
+                            refs.append(reference.as_cldf(sources[reference.key]['matchid']))
+                            srcids.add(sources[reference.key]['matchid'])
+                    else:
+                        unknown.update([ref])
+
                 args.writer.objects['ValueTable'].append(dict(
                     ID='{}-{}'.format(val['Grambank ID'], gc),
                     Value=val['Value'],
                     Language_ID=gc,
                     Parameter_ID=val['Grambank ID'],
+                    Code_ID='{}-{}'.format(val['Grambank ID'], val['Value']) if val['Value'] != '?' else None,
+                    Examples=[e['ID'] for e in examples.get('{}-{}'.format(gc, val['Grambank ID']), [])],
+                    Source=refs,
                 ))
+        for pid, vals in sorted(codes.items()):
+            for val in vals:
+                if val != '?':
+                    args.writer.objects['CodeTable'].append(dict(
+                        ID='{}-{}'.format(pid, val),
+                        Parameter_ID=pid,
+                        Name=val,
+                    ))
+        misligned = 0
+        for exs in examples.values():
+            for ex in exs:
+                if not ex['Primary_Text']:
+                    ex['Primary_Text'] = ex['Analyzed_Word']
+                ex['Analyzed_Word'] = ex['Analyzed_Word'].split()
+                ex['Gloss'] = ex['Gloss'].split()
+                if not ex['Gloss']:
+                    ex['Gloss'] = None
+                if ex['Gloss'] and len(ex['Analyzed_Word']) != len(ex['Gloss']):
+                    print(ex['ID'])
+                    print(ex['Analyzed_Word'])
+                    print(ex['Gloss'])
+                    misligned += 1
+                args.writer.objects['ExampleTable'].append(ex)
+        print(misligned)
+        for srcid in sorted(srcids):
+            src = glottolog.bibfiles[srcid]
+            src.key = srcid
+            args.writer.cldf.add_sources(str(src))
+
         for k, v in missing.most_common():
             print(k, v)
-        for code, vals in codes.items():
-            if '' in vals:
-                print(code, vals)
         return
 
     def _schema(self, cldf):
@@ -106,6 +148,7 @@ class Dataset(BaseDataset):
                 'separator': ' ',
                 'propertyUrl': 'http://cldf.clld.org/v1.0/terms.rdf#exampleReference'},
         )
+        cldf['ValueTable', 'value'].null = ['?']
         cldf.add_component('ExampleTable')
         cldf.add_component('LanguageTable')
         cldf.add_component('ParameterTable')
--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setup(
         ]
     },
     install_requires=[
+        'pyglottolog>=3.4.1',
         'cldfbench',
     ],
     extras_require={
